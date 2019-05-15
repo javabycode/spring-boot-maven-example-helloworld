443d3df210fa654929912e6423962ec7bcbbdaad
Yannick-VandenBergh <44801952+Yannick-VandenBergh@users.noreply.github.com>
2019-05-15 15:05:04 +0200

    Update HelloWorldController.java

--- src/main/java/com/javabycode/springboot/HelloWorldController.java
+++ src/main/java/com/javabycode/springboot/HelloWorldController.java
@@ -14,6 +14,6 @@ public class HelloWorldController {
         model.addAttribute("name", name);
         model.addAttribute("message", message);
         
-        return "hello from github!";
+        return "Testing Github webhook 123...";
     }
 }
